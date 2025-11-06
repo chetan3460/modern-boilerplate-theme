5b3970bca58a2d9a0a4d5084b75e3c5e3f2f4bcc
chetan dhargalkar <chetan@screenroot.com>
2025-11-06 17:29:37 +0530

    cleanup: remove unused jQuery and dead code from utilities
    utils.js:
    - Removed isInViewport(), isInViewportOffset(), inVP() - unused jQuery functions
    - Removed commented-out injectVersion() - dead code
    - Kept media query utilities and forceHMRReload() which are actively used
    
    package.json:
    - Removed jquery@^3.7.1 from dependencies (not used anywhere)
    
    assets/js/helpers/jquery.js:
    - Deleted unused file
    
    Result:
    - Main bundle reduced: 23.55 kB → 20.81 kB (-2.7 kB)
    - Cleaner codebase, no unused dependencies
    - Build successful ✓

--- assets/js/componentList.js
+++ assets/js/componentList.js
@@ -1,11 +1,4 @@
 export const componentMap = {
-  FilterSticky: {
-    mobile: true,
-    desktop: true,
-  },
-  MobileFilter: {
-    mobile: true,
-  },
   Header: {
     mobile: true,
   },
@@ -36,18 +29,14 @@ export const componentMap = {
   NewsListing: {
     mobile: true,
   },
-  CertificationSlider: {
-    mobile: true,
-  },
+
   SwiperMarquee: {
     mobile: true,
   },
   CardSlider: {
     mobile: true,
   },
-  productBlock: {
-    mobile: true,
-  },
+
   TeamMembersBlock: {
     mobile: true,
   },
@@ -57,54 +46,19 @@ export const componentMap = {
   GalleryBlock: {
     mobile: true,
   },
-  SimpleTeamTabs: {
-    mobile: true,
-  },
-  MilestonesSlider: {
-    mobile: true,
-  },
+
   CertificateModal: {
     mobile: true,
   },
-  CertificateSlider: {
-    mobile: true,
-  },
+
   RelatedBlogsSlider: {
     mobile: true,
   },
-  DistributionMap: {
-    mobile: true,
-  },
-  ProductListing: {
-    mobile: true,
-  },
-  ReportsFilter: {
-    mobile: true,
-  },
-  ReportsCategorySort: {
-    mobile: true,
-  },
-  MobileCategoriesDropdown: {
-    mobile: true,
-  },
-  TestimonialsSlider: {
-    mobile: true,
-  },
-  ProductAccordions: {
-    mobile: true,
-  },
+
   PrivacyAccordion: {
     mobile: true,
   },
   ESGSlider: {
     mobile: true,
   },
-  TestBlcok: {
-    mobile: true,
-  },
-  DistributorSalesTeamBlock: {
-    mobile: true,
-  },
 };
-
-
